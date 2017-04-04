2744c7359eac1f177b4c3aecd00476bb34747ea0
lindani <lindani@projectcodex.co>
2017-04-04 12:09:06 +0200

    update test to check returned call duration in seconds for the first number

--- tests.rb
+++ tests.rb
@@ -18,22 +18,22 @@ class TestItemisedBilling < Test::Unit::TestCase
 		 assert_equal itemised_bill.itemised_bill.size, 35  	
   end
 
-  def test_calls_for_specified_provider
+  def test_phone_calls_for_provider
   	itemised_bill = ItemisedBilling.new
   	assert_equal itemised_bill.phone_calls_for_provider('MTN')[0], "0832401145"
   	assert_equal itemised_bill.phone_calls_for_provider('Vodacom')[0], "0821302398"
   	assert_equal itemised_bill.phone_calls_for_provider('CellC')[0], "0841257809"
   end
 
-  def test_number_of_calls_made_per_provider
+  def test_provider_number_of_calls
   	itemised_bill = ItemisedBilling.new
   	assert_equal itemised_bill.provider_number_of_calls('MTN').size, 16
   	assert_equal itemised_bill.provider_number_of_calls('Vodacom').size, 8
   	assert_equal itemised_bill.provider_number_of_calls('CellC').size, 11
   end
 
-  def test_duration_convertion_in_seconds
+  def test_convert_call_duration_to_seconds
   	itemised_bill = ItemisedBilling.new
-  	assert_equal itemised_bill.convert_call_duration_to_seconds["0841257809"], [154, 334, 184, 394, 184, 334, 394, 124]
+  	assert_equal itemised_bill.convert_call_duration_to_seconds[0], ["0832401145", 334]
   end
 end
