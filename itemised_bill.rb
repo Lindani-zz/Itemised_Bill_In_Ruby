a9295d4a257db53fa3eeaa0e843a7e26de6f553f
lindani <lindani@projectcodex.co>
2017-04-04 15:21:18 +0200

    define a global class variable and set argument to a method

--- itemised_bill.rb
+++ itemised_bill.rb
@@ -3,14 +3,15 @@ require 'date'
 require 'time'
 class ItemisedBilling
 
-	def itemised_bill
-		csv = CSV.new(File.read('./ItemisedBill.csv'), headers: true)
+	@@csv_file_path ='./ItemisedBill.csv'
+
+	def itemised_bill csv_file_path
+		csv = CSV.new(File.read(csv_file_path), headers: true)
 		csv.to_a.map(&:to_h)
 	end
 
 	def phone_calls_for_provider provider
-		item_list = itemised_bill()
-
+		item_list = itemised_bill(@@csv_file_path)
 		item_list.map do |x| 
 			if x['Provider'] == provider
 				x['Number']
@@ -23,8 +24,7 @@ class ItemisedBilling
 	end
 
 	def convert_call_duration_to_seconds
-		item_list = itemised_bill()
-		       
+		item_list = itemised_bill(@@csv_file_path)       
 		item_list.map do |item|
 			dt = Time.parse("1 Jan 1970 #{item['Duration']} UTC")
 			seconds = dt.hour * 3600 + dt.min * 60 + dt.sec
